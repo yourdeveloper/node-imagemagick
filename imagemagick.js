fd2c81c71a824a2280d4d42931c9a0365003e664
dkrantsberg <dkrantsber@gannett.com>
2017-05-09 15:17:17 -0400

    changed the logic to first check whether the next line is indented and if it is then make the current property an object as opposed to a string value.

--- imagemagick.js
+++ imagemagick.js
@@ -99,36 +99,43 @@ function exec2(file, args /*, options, callback */) {
 
 
 function parseIdentify(input) {
-  var lines = input.split("\n"),
-      prop = {},
-      props = [prop],
-      prevIndent = 0,
-      indents = [indent],
-      currentLine, comps, indent, i;
+    var lines = input.split("\n"),
+        prop = {},
+        props = [prop],
+        prevIndent = 0,
+        indents = [indent],
+        currentLine, comps, indent, i, nextIndent;
 
-  lines.shift(); //drop first line (Image: name.jpg)
+    lines.shift(); //drop first line (Image: name.jpg)
 
-  for (i in lines) {
-    currentLine = lines[i];
-    indent = currentLine.search(/\S/);
-    if (indent >= 0) {
-      comps = currentLine.split(': ');
-      if (indent > prevIndent) indents.push(indent);
-      while (indent < prevIndent && props.length) {
-        indents.pop();
-        prop = props.pop();
-        prevIndent = indents[indents.length - 1];
-      }
-      if (comps.length < 2) {
-        props.push(prop);
-        prop = prop[currentLine.split(':')[0].trim().toLowerCase()] = {};
-      } else {
-        prop[comps[0].trim().toLowerCase()] = comps[1].trim()
-      }
-      prevIndent = indent;
+    for (i in lines) {
+        currentLine = lines[i];
+        indent = nextIndent || currentLine.search(/\S/);
+        if (indent >= 0) {
+            comps = currentLine.split(': ');
+            if (indent > prevIndent) indents.push(indent);
+            while (indent < prevIndent && props.length) {
+                indents.pop();
+                prop = props.pop();
+                prevIndent = indents[indents.length - 1];
+            }
+            nextIndent = getLineIndent(parseInt(i) + 1, lines);
+            if (comps.length < 2 || nextIndent > indent) {
+                props.push(prop);
+                prop = prop[currentLine.split(':')[0].trim().toLowerCase()] = {};
+            } else {
+                prop[comps[0].trim().toLowerCase()] = comps[1].trim()
+            }
+            prevIndent = indent;
+        }
     }
-  }
-  return prop;
+    return prop;
+};
+
+function getLineIndent(lineNum, lines){
+    if(lineNum < lines.length){
+        return lines[lineNum].search(/\S/);
+    }
 };
 
 exports.identify = function(pathOrArgs, callback) {
